b0fd8c67625ea10e9c708c20c8e34eda3ff511a8
Sammy Samkough <ssamkough@DCS-CORP.COM>
2020-01-29 21:53:28 -0500

    Added other social media links, fontawesome icons, updated nav as a whole

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,29 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithubAlt,
+    faLinkedinIn,
+    faTwitter,
+    faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
     <div className="footer">
       <ul className="contact">
         <li className="email">
-          <a href="mailto:sammysamkough@hotmail.com">
-            sammysamkough@hotmail.com
-          </a>
+          <a href="mailto:sammysamkough@hotmail.com"><FontAwesomeIcon icon={faEnvelope} size="lg"/></a>
         </li>
         <li className="handle">
-          <a href="https://twitter.com/ssamkough">@ssamkough</a>
+          <a target="_blank" href="https://github.com/ssamkough"><FontAwesomeIcon icon={faGithubAlt} size="lg"/></a>
+        </li>
+        <li className="handle">
+          <a target="_blank" href="https://linkedin.com/in/ssamkough"><FontAwesomeIcon icon={faLinkedinIn} size="lg"/></a>
+        </li>
+        <li className="handle">
+          <a target="_blank" href="https://twitter.com/ssamkough"><FontAwesomeIcon icon={faTwitter} size="lg"/></a>
+        </li>
+        <li className="handle">
+          <a target="_blank" href="https://instagram.com/samkotos"><FontAwesomeIcon icon={faInstagram} size="lg"/></a>
         </li>
       </ul>
     </div>
