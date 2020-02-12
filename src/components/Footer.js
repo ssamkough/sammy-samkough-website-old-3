56d0c3b7d4ee5aa04cc44b0c9c025daf8998a23f
Sammy Samkough <ssamkough@DCS-CORP.COM>
2020-02-12 16:43:02 -0500

    Refactored

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,53 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faGithubAlt,
-    faLinkedinIn,
-    faTwitter,
-    faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithubAlt,
+  faLinkedinIn,
+  faTwitter,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
     <div className="footer">
       <ul className="contact">
         <li className="email">
-          <a href="mailto:sammysamkough@hotmail.com"><FontAwesomeIcon icon={faEnvelope} size="lg"/></a>
+          <a href="mailto:sammysamkough@hotmail.com">
+            <FontAwesomeIcon icon={faEnvelope} size="lg" />
+          </a>
         </li>
         <li className="handle">
-          <a target="_blank" href="https://github.com/ssamkough"><FontAwesomeIcon icon={faGithubAlt} size="lg"/></a>
+          <a target="_blank" rel="noopener" href="https://github.com/ssamkough">
+            <FontAwesomeIcon icon={faGithubAlt} size="lg" />
+          </a>
         </li>
         <li className="handle">
-          <a target="_blank" href="https://linkedin.com/in/ssamkough"><FontAwesomeIcon icon={faLinkedinIn} size="lg"/></a>
+          <a
+            target="_blank"
+            rel="noopener"
+            href="https://linkedin.com/in/ssamkough"
+          >
+            <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
+          </a>
         </li>
         <li className="handle">
-          <a target="_blank" href="https://twitter.com/ssamkough"><FontAwesomeIcon icon={faTwitter} size="lg"/></a>
+          <a
+            target="_blank"
+            rel="noopener"
+            href="https://twitter.com/ssamkough"
+          >
+            <FontAwesomeIcon icon={faTwitter} size="lg" />
+          </a>
         </li>
         <li className="handle">
-          <a target="_blank" href="https://instagram.com/samkotos"><FontAwesomeIcon icon={faInstagram} size="lg"/></a>
+          <a
+            target="_blank"
+            rel="noopener"
+            href="https://instagram.com/samkotos"
+          >
+            <FontAwesomeIcon icon={faInstagram} size="lg" />
+          </a>
         </li>
       </ul>
     </div>
