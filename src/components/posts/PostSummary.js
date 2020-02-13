07639825c6c76cd5d2651db0af64f775437818c0
Sammy Samkough <sammysamkough@gmail.com>
2020-02-13 00:15:03 -0500

    Added post components

---
+++ src/components/posts/PostSummary.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+const PostSummary = ({ post }) => {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Body>
+        <Card.Header>{post.date}</Card.Header>
+        <Card.Title>{post.title}</Card.Title>
+        <Card.Text>{post.content}</Card.Text>
+        <Button variant="primary">View Post</Button>
+        <Card.Footer>{post.tags}</Card.Footer>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default PostSummary;
