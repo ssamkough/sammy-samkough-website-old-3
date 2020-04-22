ebc2e8e7902e6795bd5ca96a3ad735db8177f2a7
Sammy Samkough <sammysamkough@gmail.com>
2020-04-22 00:58:04 -0400

    Added commas between helpers; Made my content url friendly by making them into href's; Made sure the line breaks worked by having every line in the content go through <p>'s

--- src/pages/posts/PostDetails.tsx
+++ src/pages/posts/PostDetails.tsx
@@ -5,17 +5,52 @@ import Col from "react-bootstrap/Col";
 
 const Post = (props: any) => {
   const post = props.location.state;
+  // const content = post.content.replace(/https:\/\/[^\s]*/g, (match: string) => {
+  //   return <a href="{match}"></a>;
+  // });
   return (
     <Container>
       <br></br>
       <Row>
         <Col>
-          <h3>{post.title}</h3>
+          <div className="entity-header-text">
+            <h3>{post.title}</h3>
+          </div>
         </Col>
       </Row>
       <Row>
         <Col>
-          <div className="entity-text-wall">{post.content}</div>
+          <div className="entity-text-wall">
+            {post.content.split("\n").map((line: string, e: number) => (
+              <p className="post-content-line" key={e}>
+                {line.search(/https:\/\/[^\s]*/g) === -1 ? (
+                  <span>{line}</span>
+                ) : (
+                  <a target="_blank" rel="noopener noreferrer" href={line}>
+                    {line}
+                  </a>
+                )}
+              </p>
+            ))}
+          </div>
+          {post.helpers.length > 0 ? (
+            <div>
+              Thanks to{" "}
+              {post.helpers &&
+                post.helpers.map((helper: any) => {
+                  return (
+                    <span post-tag={helper} key={helper.toString()}>
+                      {helper}
+                      {", "}
+                    </span>
+                  );
+                })}
+              for reviewing drafts.
+            </div>
+          ) : (
+            <div></div>
+          )}
+
           <hr className="entity-details-divider" />
         </Col>
       </Row>
