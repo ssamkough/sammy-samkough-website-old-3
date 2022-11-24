3e2d8e18e531b687000f6d4eb394410f70821cde
Sammy Samkough <sammysamkough@hotmail.com>
2022-11-24 04:18:45 -0500

    adds rest of pages

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,42 @@
-const template = document.createElement("template");
-template.innerHTML = `
+const headerTemplate = document.createElement("template");
+headerTemplate.innerHTML = `
 <style>
+  header {
+    font-family: "Athelas", Calibri;
+  }
+  
+  h1 {
+    padding: 0px;
+    margin: 0px;
+  }
 
+  a {
+    transition: 0.2s ease-in;
+    text-decoration: none;
+    color: blue;
+    outline-color: purple;
+  }
+
+  a:hover {
+    filter: hue-rotate(120deg);
+  }
+
+  header {
+    font-family: "Athelas", Calibri;
+  }
+
+  header > h1 > a {
+    color: black;
+  }
+
+  header > h1 > a:hover {
+    color: crimson;
+  }
 </style>
 
 <header>
+  <h1><a href="./index.html">sammy samkough</a><span class="page-name"></span></h1>
+  <slot />
 </header>
 `;
 
@@ -12,7 +44,10 @@ class Header extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.appendChild(headerTemplate.content.cloneNode(true));
+
+    this.shadowRoot.querySelector(".page-name").innerText =
+      this.getAttribute("page-name") ? ' | ' + this.getAttribute("page-name") : "";
   }
 }
 
